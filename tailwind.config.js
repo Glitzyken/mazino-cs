02df892cae41e6b173229fa00e527c16fa90b4a6
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-12-19 20:14:57 +0100

    delete boilerplates

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+  theme: {
+    colors: {
+      mazPrime1: '#050505',
+      mazPrime2: '#2b2a28',
+      mazSec: '#e29f51',
+      mazGray1: '#d1c9be',
+      mazGray2: '#302e2f'
+    }
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  }
+};
