f033559fd6961b42c24c52e89331596952e36a1e
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-12-19 20:34:07 +0100

    set default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,74 @@
 <template>
   <div>
-    <Nuxt />
+    <!-- <PreLoader v-if="showHideSpinner" /> -->
+
+    <div class="md:bg-gradient-to-br from-mazGray1 to-mazGray2 md:py-16">
+      <div
+        class="bg-gradient-to-br from-mazPrime2 to-mazPrime1 md:max-w-6xl md:m-auto sm:rounded-lg p-2 md:p-8 text-mazGray2  md:shadow-2xl"
+      >
+        <div class="md:flex">
+          <div>
+            <!-- <TheSideBar /> -->
+          </div>
+          <div class="md:flex-1">
+            <!-- <TheNavBar /> -->
+            <Nuxt />
+          </div>
+        </div>
+      </div>
+      <div class="md:max-w-6xl md:m-auto p-2 md:p-8 text-mazGray2">
+        <!-- <TheFooter /> -->
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<!-- <script>
+export default {
+  beforeCreate() {
+    this.showHideSpinner = true;
+  },
+  mounted() {
+    this.showHideSpinner = false;
+  },
+  data() {
+    return {
+      showHideSpinner: true
+    };
+  }
+};
+</script> -->
+
+<!-- <style lang="scss">
+// GLOBAL STYLES
+.card {
+  position: relative;
+  min-height: 23rem;
+  box-shadow: 0 0.332071px 2.21381px rgba(0, 0, 0, 0.0119),
+    0 0.798012px 5.32008px rgba(0, 0, 0, 0.0258),
+    0 1.50259px 10.0172px rgba(0, 0, 0, 0.0368),
+    0 2.68036px 17.869px rgba(0, 0, 0, 0.0479),
+    0 5.01331px 33.4221px rgba(0, 0, 0, 0.0621), 0 12px 80px rgba(0, 0, 0, 0.09);
+  border-radius: 0.5rem;
+  transition: all 0.2s;
+
+  &:hover {
+    transform: translateY(-5px);
+  }
 }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
+// PAGE TRANSITION CLASS
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+.page-enter {
+  opacity: 0;
+  transform: translateX(25px);
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+.page-leave-to {
+  opacity: 0;
+  transform: translateX(-25px);
 }
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+</style> -->
