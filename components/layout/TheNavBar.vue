9847139254838a9dc9b2e035ea83d2de27e4ad07
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-12-26 08:27:38 +0100

    add dummy images

--- components/layout/TheNavBar.vue
+++ components/layout/TheNavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-between items-center">
+  <div class="flex justify-between items-center mb-10">
     <nuxt-link to="/"
       ><img class="w-10 md:w-16" src="../../assets/images/logo.png" alt="logo"
     /></nuxt-link>
--- pages/find/index.vue
+++ pages/find/index.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
-    [image] We are located at [address]
-    <a :href="origin"><button>Get direction with a map</button></a>
+    <div>
+      <img
+        class="w-2/4"
+        src="../../assets/images/studio-1.jpg"
+        alt="Mazino Makeovers Studio"
+      />
+    </div>
+    <div>
+      <p>
+        We are located at 14, Erinfolami Street, Mebamu, Off Badagary Road, Ojo, Lagos.
+      </p>
+      <a :href="origin"><button>Get direction with a map</button></a>
+    </div>
     <!--Pinnock+Beach+Estate%2+Lekki%2+EDM+Centre%2+Poise+Building%2+5A+Pinnock+Beach+Estate%2+Jakande%2+Lekki%2+Lekki+Penninsula+II%2+Lekki 
             
             6.487235998869074, 3.1700832681201323
