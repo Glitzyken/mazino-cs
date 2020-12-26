4baa633ec529cb4ee350ad3e02c5d2cd21fdfb29
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-12-26 09:06:13 +0100

    about to integrate fontawesome

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,9 @@
   <div>
     <!-- <PreLoader v-if="showHideSpinner" /> -->
 
-    <div class="md:bg-gradient-to-br from-mazGray1 to-mazGray2 md:py-16">
+    <div
+      class="md:bg-gradient-to-br from-mazGray1 to-mazGray2 md:py-16 min-h-screen"
+    >
       <div
         class="bg-gradient-to-br from-mazPrime2 to-mazPrime1 md:max-w-6xl md:m-auto p-2 md:p-8 text-mazGray1 md:shadow-2xl"
       >
--- pages/find/index.vue
+++ pages/find/index.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>
-    <div>
+  <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4">
+    <div class="col-span-2">
       <img
-        class="w-2/4"
         src="../../assets/images/studio-1.jpg"
         alt="Mazino Makeovers Studio"
       />
     </div>
-    <div>
-      <p>
-        We are located at 14, Erinfolami Street, Mebamu, Off Badagary Road, Ojo, Lagos.
+    <div class="bg-mazPrime2 px-5 py-10 text-center mt-5 md:mt-0">
+      <p class="mb-5">
+        We are located at
       </p>
-      <a :href="origin"><button>Get direction with a map</button></a>
+      <div class="bg-mazPrime1 shadow-lg">
+        <p class="p-5">
+          14, Erinfolami Street, Mebamu, Off Badagary Road, Ojo, Lagos.
+        </p>
+      </div>
+      <a :href="origin"
+        ><button
+          class="mt-20 bg-mazSec hover:bg-mazPrime3 transition duration-150 text-mazPrime1 font-bold py-2 px-5"
+        >
+          Get direction with a map 🗺
+        </button></a
+      >
     </div>
     <!--Pinnock+Beach+Estate%2+Lekki%2+EDM+Centre%2+Poise+Building%2+5A+Pinnock+Beach+Estate%2+Jakande%2+Lekki%2+Lekki+Penninsula+II%2+Lekki 
             
