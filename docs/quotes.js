11241c5abc43494cf122dd9880c9a0eef87bb403
Kenneth Jimmy <kenjimmy17@gmail.com>
2021-02-01 20:21:04 +0100

    readjust quotes ordering

--- docs/quotes.js
+++ docs/quotes.js
@@ -1,59 +1,65 @@
 export const quote1 = {
-  quote: 'Your eye makeup says so much about you and your style',
-  author: 'Laura Mercier'
+  quote:
+    'Beauty is power, and makeup is something that really enhances that; it’s a woman’s secret',
+  author: 'Charlotte Tilbury'
 };
 
 export const quote2 = {
-  quote: 'A smile is the best makeup any girl can wear',
-  author: 'Marilyn Monroe'
+  quote:
+    'That’s one of the things I love about makeup. You can change your whole attitude by just doing your eyeliner or lipstick differently',
+  author: 'Beyonce'
 };
 
 export const quote3 = {
-  quote: 'If you’re sad, add more lipstick and attack',
-  author: 'Coco Chanel'
+  quote:
+    'Treat your makeup like jewelry for the face. Play with colors, shapes, structure – it can transform you',
+  author: 'Francois Nars'
 };
 
 export const quote4 = {
-  quote: 'I love the confidence that makeup gives me',
-  author: 'Tyra Banks'
-};
-
-export const quote5 = {
-  quote: 'Beauty begins the moment you decide to be yourself',
-  author: 'Coco Chanel'
-};
-
-export const quote6 = {
-  quote: 'Joy is the best makeup. But a little lipstick is a close runner-up',
-  author: 'Anne Lamott'
-};
-
-export const quote7 = {
-  quote: 'Makeup is the finishing touch, the final accessory',
-  author: 'Marc Jacobs'
-};
-
-export const quote8 = {
-  quote: 'Creativity is your best makeup skill, don’t be afraid to experiment',
-  author: 'Pat McGrath'
-};
-
-export const quote9 = {
-  quote:
-    'You can be a woman who wants to look good and still stand up for the equality of women',
-  author: 'Meghan Markle'
-};
-
-export const quote10 = {
   quote:
     'I do put on a little makeup every day because it helps me feel put together. Mascara is essential',
   author: 'Emma Stone'
 };
 
-export const quote11 = {
+export const quote5 = {
   quote:
-    'Treat your makeup like jewelry for the face. Play with colors, shapes, structure – it can transform you',
+    'Girls have always wanted to be pretty, even in Egyptian times. Cleopatra wore all the eyeliner, you know',
+  author: 'Rita Ora'
+};
+
+export const quote6 = {
+  quote:
+    'You can be a woman who wants to look good and still stand up for the equality of women',
+  author: 'Meghan Markle'
+};
+
+export const quote7 = {
+  quote:
+    'I just like playing with makeup and clothes – so I really don’t feel like there are rules, and if there are rules, then I think it’s up to you to break them',
+  author: 'Kesha'
+};
+
+export const quote8 = {
+  quote:
+    'Foundation should unify tone, but it shouldn’t take away the individuality of the skin',
   author: 'Francois Nars'
+};
+
+export const quote9 = {
+  quote:
+    'I love being super–tough, but if I need to put on a dress and do my hair and makeup, I can do that too',
+  author: 'Elizabeth Rodriguez'
+};
+
+export const quote10 = {
+  quote: 'I love the confidence that makeup gives me',
+  author: 'Tyra Banks'
+};
+
+export const quote11 = {
+  quote: 'If you’re sad, add more lipstick and attack',
+  author: 'Coco Chanel'
 };
 
 export const quote12 = {
@@ -63,33 +69,28 @@ export const quote12 = {
 };
 
 export const quote13 = {
-  quote:
-    'That’s one of the things I love about makeup. You can change your whole attitude by just doing your eyeliner or lipstick differently',
-  author: 'Beyonce'
+  quote: 'A smile is the best makeup any girl can wear',
+  author: 'Marilyn Monroe'
 };
 
 export const quote14 = {
-  quote:
-    'I just like playing with makeup and clothes – so I really don’t feel like there are rules, and if there are rules, then I think it’s up to you to break them',
-  author: 'Kesha'
+  quote: 'Makeup is the finishing touch, the final accessory',
+  author: 'Marc Jacobs'
 };
 
 export const quote15 = {
-  quote:
-    'Beauty is power, and makeup is something that really enhances that; it’s a woman’s secret',
-  author: 'Charlotte Tilbury'
+  quote: 'Your eye makeup says so much about you and your style',
+  author: 'Laura Mercier'
 };
 
 export const quote16 = {
-  quote:
-    'Foundation should unify tone, but it shouldn’t take away the individuality of the skin',
-  author: 'Francois Nars'
+  quote: 'Creativity is your best makeup skill, don’t be afraid to experiment',
+  author: 'Pat McGrath'
 };
 
 export const quote17 = {
-  quote:
-    'Girls have always wanted to be pretty, even in Egyptian times. Cleopatra wore all the eyeliner, you know',
-  author: 'Rita Ora'
+  quote: 'Beauty begins the moment you decide to be yourself',
+  author: 'Coco Chanel'
 };
 
 export const quote18 = {
@@ -99,9 +100,8 @@ export const quote18 = {
 };
 
 export const quote19 = {
-  quote:
-    'I love being super–tough, but if I need to put on a dress and do my hair and makeup, I can do that too',
-  author: 'Elizabeth Rodriguez'
+  quote: 'Joy is the best makeup. But a little lipstick is a close runner-up',
+  author: 'Anne Lamott'
 };
 
 export const quote20 = {
