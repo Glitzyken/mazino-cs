c9ba465ce594641df685d9b3add45901e695b7ca
Kenneth Jimmy <kenjimmy17@gmail.com>
2021-02-01 20:05:38 +0100

    make quotes carousel

--- components/layout/Hero.vue
+++ components/layout/Hero.vue
@@ -1,23 +1,30 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2">
+  <div class="grid grid-cols-1 gap-12 md:gap-0 md:grid-cols-2 mt-10 md:mt-20">
     <div>
       <img src="../../assets/images/hero-woman.png" alt="hero woman" />
     </div>
     <div class="flex justify-center items-center">
-      <div class="text-center mt-5 md:mt-0">
-        <quote
-          id="quotes"
-          class="text-2xl md:text-4xl text-mazSec font-bold leading-none"
-        >
-          "{{ quotesWithAuthors[0].quote }}"
-          <span class="block text-lg md:text-2xl text-mazGray1 leading-loose">
-            – {{ quotesWithAuthors[0].author }}
-          </span>
-        </quote>
+      <div class="text-center">
+        <div class="grid grid-cols-1">
+          <VueSlickCarousel v-bind="settings">
+            <quote
+              v-for="quote in quotesWithAuthors"
+              :key="quote.author"
+              class="text-2xl md:text-4xl text-mazSec font-bold leading-none"
+            >
+              "{{ quote.quote }}"
+              <span
+                class="block text-lg md:text-2xl text-mazGray1 leading-loose"
+              >
+                – {{ quote.author }}
+              </span>
+            </quote>
+          </VueSlickCarousel>
+        </div>
         <div>
           <nuxt-link to="/about">
             <button
-              class="mt-20 bg-mazPrime1 hover:bg-mazPrime2 hover:text-mazGray1 transition-all duration-300 focus:outline-none text-mazSec font-bold py-2 px-5 capitalize"
+              class="bg-mazPrime1 hover:bg-mazPrime2 hover:text-mazGray1 transition-all duration-300 focus:outline-none text-mazSec font-bold py-2 px-5 capitalize"
             >
               why mazino?
             </button>
@@ -32,24 +39,68 @@
 </template>
 
 <script>
-import quotes from '../../docs/quotes';
+import VueSlickCarousel from 'vue-slick-carousel';
+import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+
+import {
+  quote1,
+  quote2,
+  quote3,
+  quote4,
+  quote5,
+  quote6,
+  quote7,
+  quote8,
+  quote9,
+  quote10,
+  quote11,
+  quote12,
+  quote13,
+  quote14,
+  quote15,
+  quote16,
+  quote17,
+  quote18,
+  quote19,
+  quote20,
+  quote21
+} from '../../docs/quotes';
 
 export default {
+  components: { VueSlickCarousel },
   data() {
     return {
-      quotesWithAuthors: quotes
+      settings: {
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnFocus: true
+      },
+      quotesWithAuthors: [
+        quote1,
+        quote2,
+        quote3,
+        quote4,
+        quote5,
+        quote6,
+        quote7,
+        quote8,
+        quote9,
+        quote10,
+        quote11,
+        quote12,
+        quote13,
+        quote14,
+        quote15,
+        quote16,
+        quote17,
+        quote18,
+        quote19,
+        quote20,
+        quote21
+      ]
     };
-  },
-  mounted() {
-    window.setInterval(() => {
-      this.getQuoteAndAuthor();
-    }, 5000);
-  },
-  methods: {
-    getQuoteAndAuthor() {
-      const first = this.quotesWithAuthors.shift();
-      this.quotesWithAuthors = this.quotesWithAuthors.concat(first);
-    }
   }
 };
 </script>
--- docs/quotes.js
+++ docs/quotes.js
@@ -1,100 +1,117 @@
-export default [
-  {
-    quote: 'Your eye makeup says so much about you and your style',
-    author: 'Laura Mercier'
-  },
-  {
-    quote: 'A smile is the best makeup any girl can wear',
-    author: 'Marilyn Monroe'
-  },
-  {
-    quote: 'If you’re sad, add more lipstick and attack',
-    author: 'Coco Chanel'
-  },
-  {
-    quote: 'I love the confidence that makeup gives me',
-    author: 'Tyra Banks'
-  },
-  {
-    quote: 'Beauty begins the moment you decide to be yourself',
-    author: 'Coco Chanel'
-  },
-  {
-    quote: 'Joy is the best makeup. But a little lipstick is a close runner-up',
-    author: 'Anne Lamott'
-  },
-  {
-    quote: 'Makeup is the finishing touch, the final accessory',
-    author: 'Marc Jacobs'
-  },
-  {
-    quote:
-      'Creativity is your best makeup skill, don’t be afraid to experiment',
-    author: 'Pat McGrath'
-  },
-  {
-    quote:
-      'You can be a woman who wants to look good and still stand up for the equality of women',
-    author: 'Meghan Markle'
-  },
-  {
-    quote:
-      'I do put on a little makeup every day because it helps me feel put together. Mascara is essential',
-    author: 'Emma Stone'
-  },
-  {
-    quote:
-      'Treat your makeup like jewelry for the face. Play with colors, shapes, structure – it can transform you',
-    author: 'Francois Nars'
-  },
-  {
-    quote:
-      'The most beautiful makeup for a woman is passion. But cosmetics are easier to buy',
-    author: 'Yves Saint Laurent'
-  },
-  {
-    quote:
-      'That’s one of the things I love about makeup. You can change your whole attitude by just doing your eyeliner or lipstick differently',
-    author: 'Beyonce'
-  },
-  {
-    quote:
-      'I just like playing with makeup and clothes – so I really don’t feel like there are rules, and if there are rules, then I think it’s up to you to break them',
-    author: 'Kesha'
-  },
-  {
-    quote:
-      'Beauty is power, and makeup is something that really enhances that; it’s a woman’s secret',
-    author: 'Charlotte Tilbury'
-  },
-  {
-    quote:
-      'Foundation should unify tone, but it shouldn’t take away the individuality of the skin',
-    author: 'Francois Nars'
-  },
-  {
-    quote:
-      'Girls have always wanted to be pretty, even in Egyptian times. Cleopatra wore all the eyeliner, you know',
-    author: 'Rita Ora'
-  },
-  {
-    quote:
-      'If she likes makeup, let her wear it. If she likes fashion, let her dress up. But if she doesn’t like either, let her be',
-    author: 'Chimamanda Ngozi Adichie'
-  },
-  {
-    quote:
-      'I love being super–tough, but if I need to put on a dress and do my hair and makeup, I can do that too',
-    author: 'Elizabeth Rodriguez'
-  },
-  {
-    quote:
-      'Bronzer is a booster. Choose a shade that’s one shade darker than your skin tone. It adds warmth and richness',
-    author: 'Sam Fine'
-  },
-  {
-    quote:
-      'Have a better relationship with your makeup and take the time to learn what your products will do',
-    author: 'Sam Fine'
-  }
-];
+export const quote1 = {
+  quote: 'Your eye makeup says so much about you and your style',
+  author: 'Laura Mercier'
+};
+
+export const quote2 = {
+  quote: 'A smile is the best makeup any girl can wear',
+  author: 'Marilyn Monroe'
+};
+
+export const quote3 = {
+  quote: 'If you’re sad, add more lipstick and attack',
+  author: 'Coco Chanel'
+};
+
+export const quote4 = {
+  quote: 'I love the confidence that makeup gives me',
+  author: 'Tyra Banks'
+};
+
+export const quote5 = {
+  quote: 'Beauty begins the moment you decide to be yourself',
+  author: 'Coco Chanel'
+};
+
+export const quote6 = {
+  quote: 'Joy is the best makeup. But a little lipstick is a close runner-up',
+  author: 'Anne Lamott'
+};
+
+export const quote7 = {
+  quote: 'Makeup is the finishing touch, the final accessory',
+  author: 'Marc Jacobs'
+};
+
+export const quote8 = {
+  quote: 'Creativity is your best makeup skill, don’t be afraid to experiment',
+  author: 'Pat McGrath'
+};
+
+export const quote9 = {
+  quote:
+    'You can be a woman who wants to look good and still stand up for the equality of women',
+  author: 'Meghan Markle'
+};
+
+export const quote10 = {
+  quote:
+    'I do put on a little makeup every day because it helps me feel put together. Mascara is essential',
+  author: 'Emma Stone'
+};
+
+export const quote11 = {
+  quote:
+    'Treat your makeup like jewelry for the face. Play with colors, shapes, structure – it can transform you',
+  author: 'Francois Nars'
+};
+
+export const quote12 = {
+  quote:
+    'The most beautiful makeup for a woman is passion. But cosmetics are easier to buy',
+  author: 'Yves Saint Laurent'
+};
+
+export const quote13 = {
+  quote:
+    'That’s one of the things I love about makeup. You can change your whole attitude by just doing your eyeliner or lipstick differently',
+  author: 'Beyonce'
+};
+
+export const quote14 = {
+  quote:
+    'I just like playing with makeup and clothes – so I really don’t feel like there are rules, and if there are rules, then I think it’s up to you to break them',
+  author: 'Kesha'
+};
+
+export const quote15 = {
+  quote:
+    'Beauty is power, and makeup is something that really enhances that; it’s a woman’s secret',
+  author: 'Charlotte Tilbury'
+};
+
+export const quote16 = {
+  quote:
+    'Foundation should unify tone, but it shouldn’t take away the individuality of the skin',
+  author: 'Francois Nars'
+};
+
+export const quote17 = {
+  quote:
+    'Girls have always wanted to be pretty, even in Egyptian times. Cleopatra wore all the eyeliner, you know',
+  author: 'Rita Ora'
+};
+
+export const quote18 = {
+  quote:
+    'If she likes makeup, let her wear it. If she likes fashion, let her dress up. But if she doesn’t like either, let her be',
+  author: 'Chimamanda Ngozi Adichie'
+};
+
+export const quote19 = {
+  quote:
+    'I love being super–tough, but if I need to put on a dress and do my hair and makeup, I can do that too',
+  author: 'Elizabeth Rodriguez'
+};
+
+export const quote20 = {
+  quote:
+    'Bronzer is a booster. Choose a shade that’s one shade darker than your skin tone. It adds warmth and richness',
+  author: 'Sam Fine'
+};
+
+export const quote21 = {
+  quote:
+    'Have a better relationship with your makeup and take the time to learn what your products will do',
+  author: 'Sam Fine'
+};
