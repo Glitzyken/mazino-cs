1a2bdc42fac009038b8c2a53cd3db0267e838480
Kenneth Jimmy <kenjimmy17@gmail.com>
2021-02-22 21:50:49 +0100

    Add validation to contact zino form ✔

--- components/utility/ContactForm.vue
+++ components/utility/ContactForm.vue
@@ -4,7 +4,7 @@
       class="block text-center font-bold text-4xl mb-5 leading-tight"
       for="input2"
     >
-      Send us a
+      Send Zino a
       <span class="text-5xl text-mazSec font-extrabold">
         [message]
       </span>
@@ -21,37 +21,49 @@
       <label class="text-mazSec text-xl font-bold" for="name" v-else
         >Your Name
       </label>
+      <p v-if="isEmptyName" class="text-mazSec">
+        My name is Zino. What's your name? 🙃
+      </p>
       <input
         id="name"
         class="w-full border-none px-4 py-2 rounded outline-none mb-4 bg-mazGray2"
         type="text"
         placeholder="Victoria Eloho"
-        required
+        @input="$v.name.$touch()"
         v-model="name"
       />
 
       <label class="text-mazSec text-xl font-bold" for="email"
         >Email Address</label
       >
+      <p v-if="isInvalidEmail" class="text-mazSec">
+        An invalid email won't work. 😉
+      </p>
+      <p v-if="isEmptyEmail" class="text-mazSec">
+        An empty email can't be useful, you know. 😊
+      </p>
       <input
         id="email"
         class="w-full border-none px-4 py-2 rounded outline-none mb-4 bg-mazGray2"
         type="email"
         placeholder="you@example.com"
-        required
+        @input="$v.email.$touch()"
         v-model="email"
       />
 
       <label class="text-mazSec text-xl font-bold" for="message"
         >Your Message</label
       >
+      <p v-if="isEmptyMessage" class="text-mazSec">
+        I really want to hear something from you. 😀
+      </p>
       <textarea
         id="message"
         class="w-full border-none px-4 py-2 rounded outline-none resize-none bg-mazGray2 whitespace-pre"
         type="text"
         placeholder="Hi, I think we need a colaboration. How soon can you hop on to discuss this?"
         rows="4"
-        required
+        @input="$v.message.$touch()"
         v-model="message"
       ></textarea>
 
@@ -67,26 +79,62 @@
 </template>
 
 <script>
+import { required, email } from 'vuelidate/lib/validators';
+
 export default {
   data() {
     return {
       name: '',
       email: '',
-      message: ''
+      message: '',
+      isEmptyName: false,
+      isEmptyEmail: false,
+      isEmptyMessage: false,
+      isInvalidEmail: false
     };
+  },
+  validations: {
+    name: {
+      required
+    },
+    email: {
+      required,
+      email
+    },
+    message: {
+      required
+    }
   },
   methods: {
     sendMessage() {
+      if (!this.$v.name.required) {
+        return (this.isEmptyName = true);
+      }
+      if (!this.$v.email.required) {
+        return (this.isEmptyEmail = true);
+      }
+      if (!this.$v.email.email) {
+        return (this.isInvalidEmail = true);
+      }
+      if (!this.$v.message.required) {
+        return (this.isEmptyMessage = true);
+      }
+
       const data = {
         name: this.name,
         email: this.email,
         message: this.message
       };
 
+      console.log(data);
+
       this.name = '';
       this.email = '';
       this.message = '';
-      console.log(data);
+      this.isEmptyName = false;
+      this.isEmptyEmail = false;
+      this.isEmptyMessage = false;
+      this.isInvalidEmail = false;
     }
   }
 };
